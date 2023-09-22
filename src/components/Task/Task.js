b78931e0ede146fa47c82c4a013b33d646388094
shwetapund <pundshweta2020@gmail.com>
2023-09-22 10:52:15 -0700

    added localStorage

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Task.css"
- const Task = ({id, title, description,priority}) => {
-    return(
+import img from "./images/delete.png"
+const Task = ({ id, title, description, priority, removeTaskFromList}) => {
+    return (
         <>
-        <div className="task-container">
-            <h1 className="task-title">{title}</h1>
-         <p className="task-description">{description}</p>
-           <span className="task-priority">🎯 {priority}</span>
-        </div>
+            <div className="task-container" >
+                <h1 className="task-title">{title}</h1>
+                <p className="task-description">{description}</p>
+                <span className="task-priority">🎯 {priority}</span>
+                <span>
+
+                    <img src={img} className="delete-img"
+                    onClick={() => {
+                  
+                    removeTaskFromList(id);
+                }}
+                    />
+                </span>
+            </div>
         </>
     )
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from './../../components/Task/Task'
 import './Home.css'
 const Home = () => {
@@ -10,28 +10,58 @@ const Home = () => {
             description: 'sir naraj ho jayenge',
             priority: 'high'
         },
-    
-    
+
+
     ])
 
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [priority, setPriority] = useState('')
 
+   useEffect(() => {
+    
+        const list = JSON.parse(localStorage.getItem('pinklist'));
+        setTaskList(list)    
+   }, [])
+
+    const saveListToLocalStorage = (tasks) =>{
+        localStorage.setItem('pinklist', JSON.stringify(tasks))
+    }
+
     const addTaskToList = () => {
         const randomId = Math.floor(Math.random() * 1000);
         const obj = {
-            id :randomId,
-            title: title ,
+            id: randomId,
+            title: title,
             description: description,
             priority: priority
 
         }
-        setTaskList([...taskList, obj])
+        const newTaskList = [...taskList, obj]
+        setTaskList( newTaskList)
         setTitle('');
         setDescription('');
         setPriority('');
+
+        saveListToLocalStorage(newTaskList);
     }
+    const removeTaskFromList = (id) => {
+     
+       let index;
+       taskList.forEach((task, i) => {
+        if(task.id===id){
+            index = i
+        }
+       })
+      
+        const tempArray = taskList;
+        tempArray.splice(index, 1);
+
+        setTaskList([...tempArray])
+
+        saveListToLocalStorage(tempArray);
+    }
+
     return (
         <>
             <div className='container'>
@@ -40,10 +70,19 @@ const Home = () => {
                 <div className='todo-flex-container'>
                     <div>
                         <h1 className='text-center'>Show List</h1>
-                        {
-                            taskList.map((taskItem, index) => {
-                                const { id, priority, title, description } = taskItem;
-                                return <Task id={id} priority={priority} title={title} description={description} />
+                     
+                        {taskList.map((taskItem, index) => {
+                                const { id, priority, title, description} = taskItem;
+
+                                return <Task id={id}
+                                    priority={priority}
+                                    title={title}
+                                    description={description} 
+                                    key={index}
+                                    removeTaskFromList={removeTaskFromList}
+                                   obj={taskItem}
+                                    />
+                                    
                             })
                         }
                     </div>
@@ -58,9 +97,9 @@ const Home = () => {
                                     onChange={(e) => {
                                         setTitle(e.target.value)
                                     }}
-                                    placeholder='Enter title' 
-                                    className='task-input'/>
-                        
+                                    placeholder='Enter title'
+                                    className='task-input' />
+
                                 <input type="text"
                                     value={description}
                                     onChange={(e) => {
@@ -68,14 +107,15 @@ const Home = () => {
                                     }}
                                     placeholder='Enter description'
                                     className='task-input' />
-                                
+
                                 <input type="text" value={priority} onChange={(e) => {
                                     setPriority(e.target.value)
                                 }}
-                                    placeholder='Enter priority' 
-                                    className='task-input'/>
+                                    placeholder='Enter priority'
+                                    className='task-input' />
 
-                                <button className='btn-add-task' type='button' onClick={addTaskToList}>
+                                <button className='btn-add-task' type='button'
+                                 onClick={addTaskToList}>
                                     Add Task to list
                                 </button>
                             </form>
